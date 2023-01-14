255bb962ff10a7b605985964853c1419247fa66d
natbor97 <99174052+natbor97@users.noreply.github.com>
2023-01-14 15:28:25 +0100

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -28,8 +28,13 @@ $router = new Core\Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('login', ['controller' => 'Login', 'action' => 'new']);
+
+$router->add('{controller}/{action}');
+
 $router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
 $router->add('signup/activate/{token:[\da-f]+}', ['controller' => 'Signup', 'action' => 'activate']);
-$router->add('{controller}/{action}');
-    
+
+$router->add('addExpense/getExpenseCategoryLimit/', ['controller' => 'addExpense', 'action' => 'getExpenseCategoryLimit']);
+$router->add('{controller}/{id:\d+}/{action}'); 
+
 $router->dispatch($_SERVER['QUERY_STRING']);
